9fa969564f7dc4ff54c2125276ca4844b4ee4644
chen <870267706@qq.com>
2017-08-17 20:58:53 +0800

    继承universal videoplayer

---
+++ app/src/main/java/com/example/chen/atguigucode/jiecao/JieCaoMainActivity.java
@@ -0,0 +1,19 @@
+package com.example.chen.atguigucode.jiecao;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.chen.atguigucode.R;
+
+public class JieCaoMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jie_cao_main);
+
+
+
+
+    }
+}
